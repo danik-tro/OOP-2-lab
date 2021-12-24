16bc5d7df47620a875358b3d0c30ba525ee1748c
danik-tro <daniil.trotsenko.dev@gmail.com>
2021-12-24 02:40:02 +0200

    Initial commit

---
+++ AirClub/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AirClub
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
+
